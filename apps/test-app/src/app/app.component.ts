4b70cac20c1d54a5d6b7adff1f401ac06a88d824
Emanuele Cirino <e.cirino@eco-mind.eu>
2022-01-21 12:55:53 +0100

    First app commit

---
+++ apps/test-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fe-interview-test-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'test-app';
+}
